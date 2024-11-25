541ae559fd9ef71495d30ecc4818ea36bc33c0da
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2024-11-25 08:42:23 +0000

    Code tidy

--- utils/filters.js
+++ utils/filters.js
@@ -1,28 +1,28 @@
-import { DateTime } from 'luxon'
+// import { DateTime } from 'luxon'
 
-export default {
-    dateToFormat: function (date, format) {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
-            String(format)
-        )
-    },
+// export default {
+//     dateToFormat: function (date, format) {
+//         return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
+//             String(format)
+//         )
+//     },
 
-    dateToISO: function (date) {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
-            includeOffset: false,
-            suppressMilliseconds: true
-        })
-    },
+//     dateToISO: function (date) {
+//         return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
+//             includeOffset: false,
+//             suppressMilliseconds: true
+//         })
+//     },
 
-    obfuscate: function (str) {
-        const chars = []
-        for (var i = str.length - 1; i >= 0; i--) {
-            chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
-        }
-        return chars.join('')
-    },
+//     obfuscate: function (str) {
+//         const chars = []
+//         for (var i = str.length - 1; i >= 0; i--) {
+//             chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
+//         }
+//         return chars.join('')
+//     },
 
-    filterTagList (tags) {
-        return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-    }
-}
+//     filterTagList (tags) {
+//         return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+//     }
+// }
